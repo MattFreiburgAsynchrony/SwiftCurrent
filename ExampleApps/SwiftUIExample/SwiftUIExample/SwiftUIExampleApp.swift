b507152726a3a48caa06c18d6cf8c3ce1df74c96
Richard Gist <Richard.Gist@wwt.com>
2021-11-24 09:49:39 -0700

    [skip ci] data-spike6 - Doing stuff. Also found out how to make Echo find structs and classes - RAG

--- ExampleApps/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
+++ ExampleApps/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -15,17 +15,21 @@ struct SwiftUIExampleApp: App {
     let startingWorkflow: AnyWorkflow
 
     init() {
-        DataDriven.shared.exploringEcho()
         Container.default.register(UserDefaults.self) { _ in UserDefaults.standard }
 
-//        DataDriven.shared.register(ExtendedFlowRepresentableMetadata(flowRepresentableType: SwiftCurrentOnboarding.self), for: "SwiftCurrentOnboarding")
-//        DataDriven.shared.register(key: ContentView.self, creating: ExtendedFlowRepresentableMetadata(flowRepresentableType: ContentView.self))
-//        DataDriven.register(type: LoginView.self)
-        print(DataDriven.shared.flowRepresentableViewTypes)
+        let thingToBeJson = WorkflowData(version: "0.0.1", sequence: [
+            WorkflowSequenceNode(flowRepresentableName: "SwiftCurrentOnboarding"),
+            WorkflowSequenceNode(flowRepresentableName: "ContentView"),
+        ])
 
-        let EFRMs = DataDriven.shared.flowRepresentableViewTypes.compactMap { ($0 as? TylerMetadata.Type)?.getMetadata() }
+        if let data = try? JSONEncoder().encode(thingToBeJson) {
+            let workflow = try? DataDriven.shared.createWorkflow(from: data)
+            print(workflow)
+        }
 
-        DataDriven.shared.register(EFRMs[0] as! ExtendedFlowRepresentableMetadata, for: String(describing: DataDriven.shared.flowRepresentableViewTypes[0]))
+//        let EFRMs = DataDriven.shared.flowRepresentableViewTypes.compactMap { ($0 as? TylerMetadata.Type)?.getMetadata() }
+
+//        DataDriven.shared.register(EFRMs[0] as! ExtendedFlowRepresentableMetadata, for: String(describing: DataDriven.shared.flowRepresentableViewTypes[0]))
         print(DataDriven.shared.registryDescription)
 
         do {
@@ -110,85 +114,42 @@ open class DataDriven {
         return workflow
     }
 
+    func createWorkflow(from json: Data) throws -> AnyWorkflow {
+        guard let data = try? JSONDecoder().decode(WorkflowData.self, from: json) else { throw Error.unregisteredType }
+        let workflow = AnyWorkflow.empty
+        let cachedTable = flowRepresentableViewTable
+
+        for sequence in data.sequence {
+            if let meta = (cachedTable[sequence.flowRepresentableName] as? TylerMetadata.Type)?.getMetadata() {
+                workflow.append(meta)
+            }
+        }
+
+        return workflow
+    }
+
     enum Error: Swift.Error {
         case unregisteredType
     }
 
-    // MARK: Reflection (Echo) stuff
-    func exploringEcho() -> Int {
-//        let allTypes = types
-//        let allProtocols = protocols
-
-        let structs = types.compactMap { $0 as? StructDescriptor }
-
-        let names = structs.map { $0.name }
-
-        let frProtocols = protocols.filter { $0.name.contains("FlowRepresentable") }
-
-        let overlyAbstractedMetadata = reflect(OverlyAbstracted.self) as? TypeMetadata
-
-        structs.first?.parent
-
-        // (structs[7].accessor(.complete, Any.self).metadata as? StructMetadata)?.conformances[2].protocol.name // should say FlowRepresentable
-        // (structs[7].accessor(.complete, Any.self).metadata as? StructMetadata)?.type // Should be type: Any.Type of some SwiftUIExample.OverlyAbstracted
-
-        return 0
-    }
-
-    func differentAttemptsAtConvertingFromDescriptorToType() -> [Any.Type] {
-        let views = types
-            .filter { !$0.flags.isGeneric }
-            .compactMap { $0 as? StructDescriptor }
-            .compactMap { $0.accessor(MetadataRequest(state: .abstract)) }
-            .compactMap { $0.metadata as? StructMetadata }
-            .filter { $0.conformances.contains { $0.protocol.name == String(describing: View.self) }}
-            .compactMap { $0.type }
-
-        let structDescriptors = types
-            .compactMap { $0 as? StructDescriptor }
-//            .filter { $0.parentModuleDescriptor?.name == "SwiftUIExample" }
-
-        let modules = structDescriptors
-            .compactMap { $0.parentModuleDescriptor?.name }
-        
-        let structNames = structDescriptors
-            .compactMap { $0.name }
-        print("structNames: \(structNames.count)")
-
-        let nonGenericStructs = structDescriptors
-            .filter { !$0.flags.isGeneric }
-        let nonGenericNames = nonGenericStructs
-            .compactMap { $0.name }
-        print("nonGenericNames: \(nonGenericNames.count)")
-
-        var metadataResponses = [MetadataResponse]()
-//        _ = structDescriptors
-//            .compactMap { $0.accessor(.complete, Any.self) } //.metadata as? StructMetadata }
-        for descriptor in nonGenericStructs {
-            print(descriptor.name)
-            let response = descriptor.accessor(MetadataRequest(state: .abstract))
-            metadataResponses.append(contentsOf: [response])
-        }
-
-        var metadata = [StructMetadata?]()
-        metadataResponses
-            .forEach { metadata.append($0.metadata as? StructMetadata) }
-
-        let flowRepresentables = metadata
-            .filter { $0?.conformances.contains { $0.protocol.name == String(describing: FlowRepresentable.self) } == true }
-
-
-        return flowRepresentables.compactMap { $0?.type }
-    }
-
-    var flowRepresentableViewTypes: [Any.Type] {
+    var flowRepresentableClassTypes: [Any.Type] {
         types
             .filter { !$0.flags.isGeneric }
-            .compactMap { $0 as? StructDescriptor }
+            .compactMap { $0 as? TypeContextDescriptor }
             .compactMap { $0.accessor(MetadataRequest(state: .abstract)) }
-            .compactMap { $0.metadata as? StructMetadata }
+            .compactMap { $0.metadata as? TypeMetadata }
             .filter { $0.conformances.contains { $0.protocol.name == String(describing: FlowRepresentable.self) }}
             .compactMap { $0.type }
+    }
+
+    private var flowRepresentableViewTable: [String : Any.Type] {
+        types
+            .filter { !$0.flags.isGeneric }
+            .compactMap { $0 as? TypeContextDescriptor }
+            .compactMap { $0.accessor(MetadataRequest(state: .abstract)) }
+            .compactMap { $0.metadata as? TypeMetadata }
+            .filter { $0.conformances.contains { $0.protocol.name == String(describing: FlowRepresentable.self) }}
+            .reduce(into: [:]) { $0[$1.contextDescriptor.name] = $1.type }
     }
 }
 
@@ -206,59 +167,26 @@ extension ContextDescriptor {
     }
 }
 
-protocol CombinationFRThing: View, FlowRepresentable {
-
+struct WorkflowData: Codable {
+    let version: String
+    let sequence: [WorkflowSequenceNode]
 }
 
-protocol NoOneConformsToThis {}
-
-struct OverlyAbstracted: CombinationFRThing {
-    var _workflowPointer: AnyFlowRepresentable?
-
-    var body: some View {
-        VStack {
-            Text("This thing is wack yo")
-            Button("Proceed") {
-                proceedInWorkflow()
-            }
-        }
-    }
+struct WorkflowSequenceNode: Codable {
+    let flowRepresentableName: String
+    var flowPersistence: FlowPersistenceData = .default
+    var launchStyle: LaunchStyleData = .default
 }
 
-struct GenericAbstractedView<OUTPUT> {}
+struct FlowPersistenceData: Codable {
+    static let `default` = FlowPersistenceData(type: "default")
 
-struct IndirectConformance {
-    /// The type of data coming into the `FlowRepresentable`; defaulted to `Never`; `Never`means the `FlowRepresentable` will ignore data passed in from the `Workflow`.
-    typealias WorkflowInput = Never
-    /// The type of data passed forward from the `FlowRepresentable`; defaulted to `Never`; `Never` means data will not be passed forward.
-    typealias WorkflowOutput = Never
+    let type: String
+}
 
-    /**
-     A pointer to the `AnyFlowRepresentable` that erases this `FlowRepresentable`; will automatically be set.
+struct LaunchStyleData: Codable {
+    static let `default` = LaunchStyleData(type: "default")
 
-     ### Discussion
-     This property is automatically set by a `Workflow`; it simply needs to be declared on a `FlowRepresentable`.
-     In order for a `FlowRepresentable` to have access to the `Workflow` that launched it, store the closures for proceeding forward and backward, and provide type safety. It needs this property available for writing.
-
-     #### Note
-     While not strictly necessary, it would be wise to declare this property as `weak`.
-     */
-    var _workflowPointer: AnyFlowRepresentable?
-
-    /**
-     Creates a `FlowRepresentable`.
-
-     #### Note
-     This is auto-synthesized by FlowRepresentable, and is only called when `WorkflowInput` is `Never`.
-     */
-    init() {}
-    /// Creates a `FlowRepresentable` with the specified `WorkflowInput`.
-    init(with args: WorkflowInput) {}
-
-    // No public docs necessary, as this should not be used by consumers.
-    // swiftlint:disable:next missing_docs
-    static func _factory<FR: FlowRepresentable>(_ type: FR.Type) -> FR { return Self() as! FR }
-    // No public docs necessary, as this should not be used by consumers.
-    // swiftlint:disable:next missing_docs
-    static func _factory<FR: FlowRepresentable>(_ type: FR.Type, with args: WorkflowInput) -> FR { return Self() as! FR }
+    let type: String
+    var subtype: String? = nil
 }
--- ExampleApps/UIKitExample/UIKitExample/AppDelegate.swift
+++ ExampleApps/UIKitExample/UIKitExample/AppDelegate.swift
@@ -7,8 +7,33 @@
 //
 
 import UIKit
+import Echo
+import SwiftCurrent
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     // No implementation necessary as it would only include boilerplate.
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        print(flowRepresentableClassTypes)
+    }
+
+    var flowRepresentableClassTypes: [Any.Type] {
+        types
+            .filter { !$0.flags.isGeneric }
+            .compactMap { $0 as? TypeContextDescriptor }
+            .compactMap { $0.accessor(MetadataRequest(state: .abstract)) }
+            .compactMap { $0.metadata as? TypeMetadata }
+            .filter { $0.conformances.contains { $0.protocol.name == String(describing: FlowRepresentable.self) }}
+            .compactMap { $0.type }
+    }
+
+    private var flowRepresentableViewTable: [String : Any.Type] {
+        types
+            .filter { !$0.flags.isGeneric }
+            .compactMap { $0 as? TypeContextDescriptor }
+            .compactMap { $0.accessor(MetadataRequest(state: .abstract)) }
+            .compactMap { $0.metadata as? TypeMetadata }
+            .filter { $0.conformances.contains { $0.protocol.name == String(describing: FlowRepresentable.self) }}
+            .reduce(into: [:]) { $0[$1.contextDescriptor.name] = $1.type }
+    }
 }
